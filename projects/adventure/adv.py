4dafd7f504587e04f61b7cfd38cd2276258a9dff
cslsds <lambdadatasciencelearning@gmail.com>
2020-08-14 13:35:32 -0400

    sc complete

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,9 +1,12 @@
 from room import Room
 from player import Player
 from world import World
-
 import random
+from util import Stack, Queue
 from ast import literal_eval
+from os.path import dirname, join, realpath
+
+map_file = join(dirname(realpath(__file__)), 'main_maze.txt')
 
 # Load world
 world = World()
@@ -14,7 +17,7 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,11 +28,60 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
+# Fill this out with ds to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# graph == world.rooms == {}
 
+#initialize dicts of unknown rooms
+for i in room_graph.items():
+    world.rooms[i[0]] = {}
+    dict_vals = []
+    for v in i[1][1].keys():
+        world.rooms[i[0]][v] = "?"
 
+def completionize(room): # get all unexplored rooms from cur position
+    return [possdir for possdir, label in room.items() if label == '?']
+
+def backtrack(d): # determine reverse of given direction to backtrack
+    conversion = {'n':'s', 'e':'w', 's':'n', 'w':'e'}
+    return conversion[d]
+
+def explore_rooms(player, graph):
+    run = True 
+    while run:
+        more = False # init a state for "more to explore"
+
+        # this if clause is covering case outside of while loop
+        #   in which player must backtrack to find new unexplored rooms
+        if len(traversal_path) > 0: # if we've already begun traversal
+            for i in reversed(traversal_path): # for our backtracked path
+                player.travel(backtrack(i)) # nextroom one step
+                traversal_path.append(backtrack(i)) # add backtrack step to path
+                if len(completionize(graph[player.current_room.id])) > 0:
+                # adjacent rooms remaining to explore; break back to while loop
+                    more = True
+                    break
+            if not more: # if there's no more unexplored rooms in the backtrack
+                return traversal_path # return completed path
+
+        while len(completionize(graph[player.current_room.id])) > 0:
+        # while adjacent rooms remain left to explore        
+                room_id = player.current_room.id
+                # based on room player is in,
+                unexplored = completionize(graph[room_id])
+                # identify unexplored rooms
+                nextroom = random.choice(unexplored)
+                player.travel(nextroom)
+                # and travel to one of them
+                graph[room_id][nextroom] = player.current_room.id
+                # update graph with this new room's id
+                graph[player.current_room.id][backtrack(nextroom)] = room_id
+                # and update the graph with the 'path' back to the previous room
+                traversal_path.append(nextroom)
+                # and add the latest turn to the traversal path
+
+explore_rooms(player, world.rooms)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +103,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
